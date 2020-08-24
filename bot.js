b290531a8975ea2a09ca35e37a8c88e7b61f0add
Pablo Escobar <kooshpatel@gmail.com>
2020-08-24 17:28:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+//hosted on Heroku :)
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
@@ -57,6 +59,11 @@ client.on('message', message => {
                                 //pretty print everything
                                 console.log(info)
                                 message.reply(t);
+                                output = t + " Quote: \n" + 'Open: ' + String(info[0]['o']) + 'High: ' + String(info[0]['h']) + 'Low: ' + String(info[0]['l']) + 'Close: ' + String(info[0]['c']) + 'Previous Close: ' + String(info[0]['pc'])
+                                output = output + '\n'
+                                output = output + 'Recommendations as of ' + info[1][0]['period'] + ' Buy: ' + String(info[1][0]['buy']) + ' Sell: ' + String(info[1][0]['sell']) + ' Hold: ' + String(info[1][0]['hold']) + '\n'
+                                output = output + 'Aggregate Indicators: ' + info[2]['technicalAnalysis']['signal'] + '\n'
+                                output = output + 'Trends: ' + String(info[2]['trend']['adx']) + ',  Trending = ' + String(info[2]['trend']['trending'])
                             });
                         
                     }});
